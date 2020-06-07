4f011464bf74ecd51040cf24753be37d37e51e8a
StephenRansom <stephen.ransom@pm.me>
2020-06-07 17:31:22 -0600

    1.1 comments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
     println!("Hello, world!");
     println!("It's weird that println has an exclamation mark after it");
+    comments();
+
 }
+
+
+/*
+* 1.1 comments
+ */
+fn comments(){
+    let x= 5+ /* *90 + */ 5;
+    println!("x = {}", x);
+}
+
