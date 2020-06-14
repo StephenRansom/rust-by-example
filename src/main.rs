342b99634042cb17b93a1af9e0d57eb1a709cbf3
StephenRansom <stephen.ransom@pm.me>
2020-06-14 15:12:06 -0600

    added more chapter two primative type work

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+/**
+* If you're reading this, you must be very bored. There's not really much substantive in this pile of code
+* I'm just learning rust syntax in my free time, and creating random puddles of code just for practice.
+* check out https://doc.rust-lang.org/stable/rust-by-example/ if you want to learn rust, because this
+* program is likely not going to help you.
+*/
+
 
 fn main() {
     // println!("Hello, world!");
-    // println!("It's weird that println has an exclamation mark after it");
     // chapter_one::comments();
     // chapter_one::formatted_print();
-    chapter_two::scalar_types();
+    // chapter_two::scalar_types();
+    chapter_two::compound_types();
 }
 
 
@@ -52,12 +59,23 @@ mod chapter_one{
     }
 }
 
-
+#[allow(dead_code)]
 mod chapter_two{
     pub fn scalar_types(){
-        //signed ints
+        print_ints();
+        print_floats();
+        print_char_bools();
+        literals_and_operators();
+    }
+
+    pub fn compound_types(){
+        array_stuff();
+        tuple_trouble();
+    }
+
+    fn print_ints(){
         let eight: i8 = 127;
-        let ueight: u8 = 255;
+        let ueight = 255u8;  //suffix annotation, do not like
         let sixteen:i16 = 32767;
         let usixteen:u16= 65535;
         let thirty_two:i32 = 2147483647;
@@ -74,14 +92,65 @@ mod chapter_two{
         println!("32\t {}\t\t\t\t\t\t\t\t\t{}\t", thirty_two, uthirty_two);
         println!("64\t {}\t\t\t\t\t\t{}\t", sixty_four, usixty_four);
         println!("128\t {}\t{}\t", one_twenty_eight, u_one_twenty_eight);
-
-
-
-
     }
 
+    fn print_floats(){
+        let three : f32 = 3.3;
+        let pie : f64 = 3.14455555555555555555555555555555555555555555555555555555555555;
+        println!("{:.20},{:.20}",three, pie);
+    }
+
+    fn print_char_bools(){
+        let k: char = 'k';
+        let bob_bool: bool = true;
+        if(bob_bool){
+            println!("Bob's favorite letter is {}", k);
+        }else{
+            println!{"Who knows what his favorite letters is, and who really cares anyway?"};
+        }
+    }
+
+    fn literals_and_operators(){
+        // Integer addition
+        println!("1 + 2 = {}", 1u32 + 2);
+
+        // Integer subtraction
+        println!("1 - 2 = {}", 1i32 - 2);
+
+        // Short-circuiting boolean logic
+        println!("true AND false is {}", true && false);
+        println!("true OR false is {}", true || false);
+        println!("NOT true is {}", !true);
+
+        // Bitwise operations
+        println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+        println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+        println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+        println!("1 << 5 is {}", 1u32 << 5);
+        println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+        // Use underscores to improve readability!
+        println!("One million is written as {}", 1_000_000u32);
+    }
+
+    fn array_stuff(){
+        //no messing around with multi type arrays, use a tuple
+        // let an_array = [1,4,3,76,567,3456,true, "asdf"];
+        let bool_array:[bool; 2] = [true, false];
+        let an_array = [1,2,3,4,5,6];
+        //can't do this, gotta read the array chapter to figure out if you can make them mutable
+        // an_array=[1]=4;
+        // println!("{}", an_array);
+    }
+
+    fn tuple_trouble(){
+        let tribble = ("troublesome", true, false);
+        println!("True or false: tribbles are {} ", tribble.0);
+        println!("{}",tribble.1);
+    }
 }
 
+#[allow(dead_code)]
 mod chapter_five{
     fn five_point_one(){
 
